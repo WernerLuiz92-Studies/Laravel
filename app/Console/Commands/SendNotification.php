ba1968850349f03f41047a8e86afc1371196d3a2
Werner Luiz Gottschalt <wgottschalt.studies@gmail.com>
2022-01-20 17:06:54 -0300

    feat(Notification): Criado 10000 usuários, Criado Comando para notificar por email, criado notificação, criado job para notificação para enviar pela fila

---
+++ app/Console/Commands/SendNotification.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class SendNotification extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'notify:all';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->withProgressBar(
+            User::all(),
+            fn (User $user) => $user->notify(
+                new \App\Notifications\MaintenanceNotice()
+            )
+        );
+    }
+}
---
+++ app/Jobs/SendNotificationsJob.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+class SendNotificationsJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        User::all()->each(
+            fn (User $user) => $user->notify(
+                new \App\Notifications\MaintenanceNotice()
+            )
+        );
+    }
+}
